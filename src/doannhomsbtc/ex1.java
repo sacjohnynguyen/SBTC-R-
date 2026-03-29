c0dc18534ff2abc9945f2946c84922d99dbb138f
nhdeptrai1 <nngocsang098@gmail.com>
2026-03-29 20:47:28 +0700

    Tim Max

--- src/doannhomsbtc/ex1.java
+++ src/doannhomsbtc/ex1.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package doannhomsbtc;
 
-/**
- *
- * @author Windows
- */
+package doannhomsbtc;
+import java.util.*;
 public class ex1 {
+    int []a;
+    void sinhMang(int n){
+        Random rd = new Random();
+        a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = rd.nextInt(1,10);
+        }
+    }
+    void in(String t){
+        System.out.println("\n"+ t);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+    void timMax() {
+        if (a == null || a.length == 0) {
+            System.out.println("Mang rong!");
+            return;
+        }
+        
+        int max = a[0]; 
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i]; 
+            }
+        }
+        System.out.println("\nGia tri lon nhat trong mang la: " + max);
+    }
+    public static void main(String[] args) {
+        ex1 l = new ex1();
+        l.sinhMang(10);
+        l.in("Hien thi mang");
+        l.timMax();
+    }
     
 }
