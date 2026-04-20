4f9b190efec23f77df3241fa555a6c21fcf407b0
Hoàng Minh Đức <hoangminhduc30106@gmail.com>
2026-04-20 22:43:26 +0700

    sua sapxepgd

--- src/doannhomsbtc/ex3.java
+++ src/doannhomsbtc/ex3.java
@@ -16,7 +16,7 @@ public class ex1 {
         }
     }
     void sapXepGiamDan() {
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < a.length - 1; i--) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] < a[j]) {
                     int temp = a[i];
